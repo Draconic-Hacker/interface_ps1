fc6507d7a68f6bdb86525577c8b907cc2ef1c905
Draconic-Hacker <pythontesteemail55@gmail.com>
2026-03-31 14:20:58 -0300

    Refactor update script to remove unnecessary commands

--- update.ps1
+++ update.ps1
@@ -1,18 +1,12 @@
 cd $env:TEMP
 
-taskkill /fi "windowtitle eq Meu Gerenciador" /f
-
-cls
-
 $installDir = Join-Path $HOME "browser-files"
-$batFile = Join-Path $installDir "navegador.bat"
-$urlMain = "https://raw.githubusercontent.com/Draconic-Hacker/scripts-batch/refs/heads/master/navegador.bat"
+$interfaceFile = Join-Path $installDir "interfaceGui.ps1"
+$urlInterface = "https://raw.githubusercontent.com/Draconic-Hacker/interface_ps1/refs/heads/master/interfaceGUI.ps1"
 
 Write-Host "Fazendo a atualizacao dos arquivos de Browser-Files..." -ForegroundColor Cyan
+
+Invoke-WebRequest -Uri $urlInterface -OutFile $interfaceFile
+
 write-host "`nAutalizacoes concluidas, pressione Enter para abrir." -Foregroundcolor Green
 Read-Host | Out-Null
-
-$navegadorContent = Invoke-WebRequest -Uri $urlMain -UseBasicParsing
-[System.IO.File]::WriteAllText($batFile, $navegadorContent.Content, [System.Text.Encoding]::ASCII)
-
-start cmd "/k cd $home/browser-files & navegador.bat"
